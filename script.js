5d34d23ef375368febbc08e62497d720c0442a87
Maria Luiza Cardoso <167557650+malu-car@users.noreply.github.com>
2024-04-19 13:56:33 -0300

    Relógio digital.
    Relógio digital feito com HTML, CSS e JavaScript.

---
+++ script.js
@@ -0,0 +1,17 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let h = dateToday.getHours();
+    let m = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+    horas.textContent = h;
+    minutos.textContent = m;
+    segundos.textContent = s;
+
+    if (h < 10 )h = '0' + h;
+    if (m < 10 )m = '0' + m;
+    if (s < 10 )s = '0' + s;
+})
